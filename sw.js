7d7ae03377873aebab7529015d03db8d09ccc7ca
psdevraj-creator <psdevraj-creator@users.noreply.github.com>
2026-07-11 18:41:04 +0100

    Remove stale sidebar CSS, add global search, fix content max-width

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "bhtalks-20260711-183155";
+var CACHE = "bhtalks-20260711-184055";
 var ASSETS = ["index.html","manifest.json","icons/icon-192.png","icons/icon-512.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
