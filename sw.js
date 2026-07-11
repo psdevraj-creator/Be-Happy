5b6b34bdc5c9cba8a15493fe550a28a93717d674
psdevraj-creator <psdevraj-creator@users.noreply.github.com>
2026-07-11 22:01:46 +0100

    Fix transcript lookup: global scan instead of folder-name match (+156 transcripts found)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "bhtalks-20260711-215142";
+var CACHE = "bhtalks-20260711-220142";
 var ASSETS = ["index.html","manifest.json","icons/icon-192.png","icons/icon-512.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
